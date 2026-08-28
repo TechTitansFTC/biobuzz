d8cee5f4efd9cc4641fb4d11439adf8f259bea44
siddman2020 <siddman2020@gmail.com>
2026-08-28 18:47:44 -0500

    added DriveCommand

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/DriveCommand.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.seattlesolvers.solverslib.command.CommandBase;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveSubsystem;
+
+public class DriveCommand extends CommandBase {
+    private final MecanumDriveSubsystem drive;
+    private final GamepadEx gamepad;
+    String team;
+    public DriveCommand(GamepadEx gamepad, MecanumDriveSubsystem drive, String team) {
+        this.drive = drive;
+        this.gamepad = gamepad;
+        this.team = team;
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        double x = gamepad.getLeftX();
+        double y = gamepad.getLeftY();
+        double rotation = gamepad.getRightX();
+
+        drive.drive(x, y, rotation);
+    }
+
+    public void changeTeam(String team) {this.team = team;}
+
+    @Override
+    public void end(boolean interrupted) { drive.drive(0, 0, 0); }
+}
