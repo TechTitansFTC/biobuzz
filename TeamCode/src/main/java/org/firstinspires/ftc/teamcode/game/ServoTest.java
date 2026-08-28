f0fa128e61b696f2958fcf0a1954aca922f4214a
Abhi-karthi <164704465+Abhi-karthi@users.noreply.github.com>
2026-08-28 18:33:52 -0500

    renamed folders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/game/ServoTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Game;
+package org.firstinspires.ftc.teamcode.game;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
