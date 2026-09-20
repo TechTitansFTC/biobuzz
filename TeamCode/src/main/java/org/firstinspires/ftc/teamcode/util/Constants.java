a19233bce95350675a4274da352ed1754a70a4be
Abhi-karthi <karthiicode@icloud.com>
2026-09-20 14:27:43 -0500

    remov4ed all intake sh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
@@ -12,24 +12,6 @@ public class Constants {
     public static RevHubOrientationOnRobot.LogoFacingDirection controlHubLogoFacingDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
     public static RevHubOrientationOnRobot.UsbFacingDirection controlHubUSBFacingDirection = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
 
-    // Intake
-    public static Servo.Direction intakeWheelPivotServoLeftDirection = Servo.Direction.FORWARD;
-    public static Servo.Direction intakeWheelPivotServoRightDirection = Servo.Direction.REVERSE;
-    public static Servo.Direction intakeRampPivotServoLeftDirection = Servo.Direction.FORWARD;
-    public static Servo.Direction intakeRampPivotServoRightDirection = Servo.Direction.REVERSE;
-    public static Servo.Direction intakeFlowerPivotServoLeftDirection = Servo.Direction.FORWARD;
-    public static Servo.Direction intakeFlowerPivotServoRightDirection = Servo.Direction.REVERSE;
-    public static DcMotor.Direction intakeMotorDirection = DcMotor.Direction.REVERSE;
-
-    public static double intakeWheelPivotPositionUp = 0.3;
-    public static double intakeWheelPivotPositionDown = 0.4;
-    public static double intakeRampPivotPositionUp = 0.3;
-    public static double intakeRampPivotPositionDown = 0.4;
-    public static double intakeFlowerPivotPositionUp = 0.3;
-    public static double intakeFlowerPivotPositionDown = 0.4;
-
-    public static double intakeMotorPower = 0.4;
-
     // Shooter
     public static DcMotor.Direction shooterMotorLeftDirection = DcMotor.Direction.FORWARD;
     public static DcMotor.Direction shooterMotorRightDirection = DcMotorSimple.Direction.REVERSE;
